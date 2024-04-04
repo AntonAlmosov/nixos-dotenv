c560dffd6eeb43b28a9d2c3bad69cde8fb55e8d9
Anton Almosov <almosoww@gmail.com>
2024-04-04 16:33:28 +0000

    feat: mini files

--- programs/neovim/nvim/lua/plugins/neotree.lua
+++ programs/neovim/nvim/lua/plugins/neotree.lua
@@ -1,17 +1,4 @@
 return {
-  "neo-tree.nvim",
-  opts = {
-    filesystem = {
-      filtered_items = {
-        visible = true,
-        hide_dotfiles = false,
-        hide_gitignored = true,
-      },
-    },
-    window = {
-      mappings = {
-        ["l"] = "open",
-      },
-    },
-  },
+	"neo-tree.nvim",
+	enabled = false,
 }
--- programs/neovim/nvim/lua/plugins/nvim-tmux-navigator.lua
+++ programs/neovim/nvim/lua/plugins/nvim-tmux-navigator.lua
@@ -1,36 +1,15 @@
 return {
-  "alexghergh/nvim-tmux-navigation",
-  keys = {
-    {
-      "<C-l>",
-      function()
-        require("nvim-tmux-navigation").NvimTmuxNavigateRight()
-      end,
-      desc = "tmux navigate right",
-    },
-    {
-      "<C-h>",
-      function()
-        require("nvim-tmux-navigation").NvimTmuxNavigateLeft()
-      end,
-      desc = "tmux navigate left",
-    },
-    {
-      "<C-j>",
-      function()
-        require("nvim-tmux-navigation").NvimTmuxNavigateDown()
-      end,
-      desc = "tmux navigate down",
-    },
-    {
-      "<C-k>",
-      function()
-        require("nvim-tmux-navigation").NvimTmuxNavigateUp()
-      end,
-      desc = "tmux navigate up",
-    },
-  },
-  config = function()
-    require("nvim-tmux-navigation").setup()
-  end,
+	"alexghergh/nvim-tmux-navigation",
+	config = function()
+		require("nvim-tmux-navigation").setup({
+			keybindings = {
+				left = "<C-h>",
+				down = "<C-j>",
+				up = "<C-k>",
+				right = "<C-l>",
+				last_active = "<C-\\>",
+				next = "<C-Space>",
+			},
+		})
+	end,
 }
